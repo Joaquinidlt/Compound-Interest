11bd1aab0fc39e19283ded4f47c7c85f5654d7fa
joaquinidlt <joaquindlt16@gmail.com>
2022-10-02 19:53:20 +0200

    readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Compound-Interest
+
+
+With this application, you will be able to get the compound interest of your investments.
+
+
+
+
+
+!<p align="center">
+  <img  src="./img/screenCompoundInterest.png" />
+</p>
+
+!<p align="center">
+  <img  src="./img/screenCompoundInterest1.png" />
+</p>
--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// Future Value
-/* let investment = [];
-let annualRate = [];
-let years = [];
-let futureValue = []; */
+
 
 const futureValue = document.querySelector('#eventInitialCap');
 const investment = document.querySelector('#eventAnnualAdd');
@@ -13,15 +9,13 @@ const result = document.querySelector('#result');
 
 
 document.querySelector('form').addEventListener('submit', (e) => { // saco la referencia a mi "form" y luego con "addEventListener" le dijo que cuando haga click em submit, ejecute la siguiente funcion.
+
     e.preventDefault();
+
     let futureValue1 = parseInt(futureValue.value);
     let investment1 = parseInt(investment.value);
     let years1 = parseInt(years.value);
     let annualRate1 = parseInt(years.value);
-    console.log(futureValue1)
-    console.log(investment1)
-    console.log(years1)
-    console.log(annualRate1)
 
     if (futureValue1.value === "" || investment1.value === "" || annualRate1 === "" || years1 === "") {
         return;
@@ -33,7 +27,6 @@ document.querySelector('form').addEventListener('submit', (e) => { // saco la re
         futureValue1 = ((futureValue1 + investment1) * (1 + monthlyRate)).toFixed(2);
     }
     result.innerHTML = `$ ${futureValue1}`;
-    console.log(futureValue1)
 
 });
 
